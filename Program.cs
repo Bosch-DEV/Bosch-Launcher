3ebe3005e74b49492b69ab054679a83e7f73b03a
Bedlessdeus <bedlesssgod@gmail.com>
2024-09-05 16:19:17 +0200

    Added Window

--- Program.cs
+++ Program.cs
@@ -2,25 +2,18 @@
 using static Ignite.Console;
 
 
-var index = 0;
-var rounds = 1;
-
-#if !DEBUG
-while (true) {
-    try {
-        index = Scan<int> ("An welchem Monitor soll das Spiel geöffnet werden (<foreground-#E26A44>bei keiner Eingabe wird der Primäre monitor verwendet</foreground>): ") - 1;
-
-        if (index < 0 || index >= Monitors.Count)
-            throw new Exception ();
-
-        break;
-    } catch {
-        Printl ($"    <foreground-red>Die Eingabe war ungültig, bitte versichern Sie sich, dass die Eingabe zwischen 1 und {Monitors.Count + 1}!</foreground>");
-    }
-}
-#endif
-
-var instance = new Draw (index);
+var display = 1;
+Window window = new Window(1);
+/*
+var instance = new Draw (display);
+var heightCal = ((Monitors[display].Resolution.Height / 100) * 70);
+var widthCal = ((Monitors[display].Resolution.Width / 100) * 70);
+var startY = (Monitors[display].Resolution.Height / 100) * 15;
+var startX = (Monitors[display].Resolution.Width /100) * 15;
+var main = new Rectangle(startX, startY, widthCal, heightCal, color: Color.Yellow, fill: true);
+instance.Append(main);
+*/
+/*
 var length = 300;
 var x = (Monitors[index].Resolution.Width >> 1) - (length + (length >> 1));
 var y = (Monitors[index].Resolution.Height >> 1) - (length + (length >> 1));
@@ -34,5 +27,6 @@ for (var i = 0; i < length * 3; i += length) {
 }
 
 instance.Append (new Text (x + Text.Width ($"Round 1/{rounds}") + length, y - 25, $"Round 1/{rounds}"));
+*/
 
 Console.ReadLine ();
---
+++ Window.cs
@@ -0,0 +1,69 @@
+﻿using static Ignite.Screen;
+using static Ignite.Console;
+
+public class Window {
+
+    private const int defaultDisplay = 0;
+
+    private int display = 1;
+
+    private Draw instance;
+
+    private int height;
+    private int width;
+
+    private int startLocationX;
+    private int startLocationY;
+
+    private uint backgroundColor;
+
+    private Rectangle mainContainer;
+
+	public Window(int display = defaultDisplay, uint color = Color.Gray) {
+        this.display = display;
+        this.instance = new Draw(display);
+        this.height = this.CalculateHeight();
+        this.width = this.CalculateWidth();
+        this.startLocationX = this.CalculateStartX();
+        this.startLocationY = this.CalculateStartY();
+        this.backgroundColor = color;
+        this.mainContainer = this.CreateMainWindow();
+    }
+
+    //Removes the old container, and creates a new one
+    public Rectangle CreateMainWindow() {
+        _ = this.DestructMainWindow();
+        var mainR = new Rectangle(this.CalculateStartX(), this.CalculateStartY(), this.CalculateWidth(), this.CalculateHeight(), this.backgroundColor, fill: true);
+        this.instance.Append(mainR);
+        return mainR;
+    }
+
+    //Erases everything drawn by said instance
+    public Window DestructMainWindow() {
+        this.instance.Erase();
+        this.instance = new Draw(display);
+        return this;
+    }
+    
+    /*Window Size Calculators*/
+    // Height / Y Calc
+    public int CalculateHeight(int percentage) => Monitors[this.display].Resolution.Height / 100 * percentage;
+
+    public int CalculateHeight() => this.CalculateHeight(70);
+
+    // Width / X Calc
+    public int CalculateWidth(int percentage) => Monitors[this.display].Resolution.Width / 100 * percentage;
+
+    public int CalculateWidth() => this.CalculateWidth(70);
+    
+    /*Start Location Calculators*/
+    // Y Calculator
+    public int CalculateStartY(int percentage) => Monitors[this.display].Resolution.Height / 100 * percentage;
+
+    public int CalculateStartY() => this.CalculateStartY(15);
+
+    // X Calculator
+    public int CalculateStartX(int percentage) => Monitors[this.display].Resolution.Width /100 * percentage;
+
+    public int CalculateStartX() => this.CalculateStartX(15);
+}
